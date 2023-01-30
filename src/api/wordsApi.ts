6ee8c30f6ddff2062a50df75148080e18f0aaf18
Oleksandr Radoshniuk <radosh.dev@gmail.com>
2023-01-30 12:26:57 +0200

    change words link

--- src/api/wordsApi.ts
+++ src/api/wordsApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const wordsUrl =
-	'https://gist.githubusercontent.com/radoshDev/d3af3a2be5403786f67dcdc72a39e38e/raw/37ee77e4e4f64cf6a6cc18842a0589c5b334751f/ukrainian-words.csv'
+	'https://gist.githubusercontent.com/radoshDev/d3af3a2be5403786f67dcdc72a39e38e/raw/d6749c181e546e6293510db06262ca8db4660390/ukrainian-words.csv'
 
 export const getWords = async (): Promise<string> => {
 	const result = await axios.get<string>(wordsUrl)
