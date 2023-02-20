7ab4158b80c349f95600747c506305735774f544
Oleksandr Radoshniuk <radosh.dev@gmail.com>
2023-02-20 13:46:48 +0200

    add words local

--- src/api/wordsApi.ts
+++ src/api/wordsApi.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
-const wordsUrl =
-	'https://gist.githubusercontent.com/radoshDev/d3af3a2be5403786f67dcdc72a39e38e/raw/d6749c181e546e6293510db06262ca8db4660390/ukrainian-words.csv'
+const wordsUrl = '/db/words.csv'
 
 export const getWords = async (): Promise<string> => {
 	const result = await axios.get<string>(wordsUrl)
