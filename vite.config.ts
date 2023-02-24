591b12c701643957b75767f24eaff8a49b01f51d
Oleksandr Radoshniuk <radosh.dev@gmail.com>
2023-02-24 17:29:02 +0200

    fix vitest coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
+import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { VitePWA } from 'vite-plugin-pwa'
